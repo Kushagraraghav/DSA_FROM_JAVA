9f131168c26ab993c0a2a4de176000df1c4cc87c
Kushagra Raghav <152775429+Kushagraraghav@users.noreply.github.com>
2024-10-21 23:14:00 +0530

    Array

---
+++ DSA FROM JAVA/ARRAYS/SumOfArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfArray {
+    
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
