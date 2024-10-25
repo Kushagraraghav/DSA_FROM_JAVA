3612a25d11bb8005dcbbcb52ac1221251bd5bdf2
Kushagra Raghav <152775429+Kushagraraghav@users.noreply.github.com>
2024-10-25 22:38:33 +0530

    DSA_FROM_JAVA

---
+++ DSA FROM JAVA/RotateMatrix90.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class RotateMatrix90 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the square matrix (n x n): ");
+        int n = scanner.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        rotate90(matrix, n);
+
+        System.out.println("Matrix after 90-degree rotation:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void rotate90(int[][] matrix, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
+
+
